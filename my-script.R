e977b0370a0687a93da3edd67e765a621fed7fe3
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2026-05-07 11:26:19 -0400

    test token process

--- my-script.R
+++ my-script.R
@@ -3,3 +3,10 @@
 message("Hello from R!")
 
 writeLines(as.character(Sys.time()), "my-data.txt")
+
+if(file.exists("tokenfile.RDS")) {
+    message("token file exists!")
+} else {
+    stop("no token file :(")
+}
+
