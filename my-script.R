9aadc85215a9e0938a1500b872263b54c0cc43d9
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2026-06-02 20:43:14 -0400

    Update my-script.R

--- my-script.R
+++ my-script.R
@@ -1,8 +1,6 @@
 # Example script
 library(rdrop2refreshtoken)
 
-datadir <- ""
-
 message("Hello from R!")
 
 writeLines(as.character(Sys.time()), "my-data.txt")
@@ -13,7 +11,7 @@ if(file.exists("tokenfile.RDS")) {
     stop("no token file :(")
 }
 
-rdrop2refreshtoken::drop_auth(new_user = FALSE, rdstoken = "tokenfile.RDS") -> token
+rdrop2refreshtoken::drop_auth(new_user = FALSE, rdstoken = "tokenfile.RDS")
 
 print(rdrop2refreshtoken::drop_dir())
 
